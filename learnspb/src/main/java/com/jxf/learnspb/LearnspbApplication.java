a3d6364dcf86b18af9afa0c742bc5aad0f1897ad
jinxiaofeng0822 <jinxf0822@foxmail.com>
2018-02-17 21:13:46 +0800

    测试密码

--- learnspb/src/main/java/com/jxf/learnspb/LearnspbApplication.java
+++ learnspb/src/main/java/com/jxf/learnspb/LearnspbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnspbApplication {
-
+	//测试密码
 	public static void main(String[] args) {
 		SpringApplication.run(LearnspbApplication.class, args);
 	}
